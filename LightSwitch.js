0befc7dacb0dd9ff24373fe3f1f7a8f93abc15b8
Mohammed M. Fituri <mfituri94@gmail.com>
2017-11-19 22:11:23 +0300

    completed LightSwitch

--- LightSwitch.js
+++ LightSwitch.js
@@ -6,13 +6,50 @@ const GenericToggle = require('./presentational/GenericToggle');
 
 
 type PropsType = {};
-type StateType = {};
+type StateType = {
+  intensity: number
+};
 
 class LightSwitch extends React.Component<PropsType, StateType> {
 
+  state = {
+    intensity: 0
+  }
+
+  /* props to be passed to GenericToggle */
+  _values : [string, string] = [
+    'On',
+    'Off'
+  ];
+
+  _actions : [() => null, () => null] = [
+    this.turnOn.bind(this),
+    this.turnOff.bind(this)
+  ];
+
+  turnOn() {
+    this.setState({
+      intensity: 1
+    });
+  }
+
+  turnOff() {
+    this.setState({
+      intensity: 0
+    });
+  }
+
   render() {
+    const { intensity } = this.state;
+
+    var selected = 0;
+    if (!intensity) {
+      selected = 1;
+    }
+
     return (
-      <GenericToggle />
+      <GenericToggle selected={selected}
+        values={this._values} actions={this._actions} />
     );
   }
 }
