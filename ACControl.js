4760f37b49a1e0adc91e43dd4c2912d791933c97
Mohammed M. Fituri <mfituri94@gmail.com>
2017-11-22 20:01:40 +0300

    completed GenericCircularSlider and basic ACControl

---
+++ ACControl.js
@@ -0,0 +1,86 @@
+/* @flow */
+
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const GenericCircularSlider = require('./presentational/GenericCircularSlider');
+const GenericToggle = require('./presentational/GenericToggle');
+
+class ACControl extends React.Component<PropsType, StateType> {
+
+  state = {
+    fan_speed: 1,
+    temperature: 22,
+  };
+
+  _fan_speeds = [
+    'Off',
+    'Low',
+    'High'
+  ];
+
+  _fan_actions = [
+    () => this.changeFan(0),
+    () => this.changeFan(1),
+    () => this.changeFan(2)
+  ];
+
+  changeFan(speed: number) {
+    this.setState({
+      fan_speed: speed
+    });
+  }
+
+  temperatureChange(temperature: number) {
+    this.setState({
+      temperature
+    });
+  }
+
+  round(value: number) {
+    return (Math.round(value * 2) / 2).toFixed(1);
+  }
+
+  formatText(text: string) {
+    const { fan_speed } = this.state;
+
+    if (fan_speed) {
+      return text + '°C';
+    }
+
+    return 'Off';
+  }
+
+  render() {
+    const { fan_speed, temperature, room_temperature } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <GenericCircularSlider value={temperature}
+          minimum={16} maximum={30}
+          round={this.round.bind(this)}
+          formatText={this.formatText.bind(this)}
+          onRelease={this.temperatureChange.bind(this)}/>
+        <GenericToggle values={this._fan_speeds}
+          actions={this._fan_actions} selected={fan_speed} />
+        <Text style={styles.room_temperature}>
+          Room Temperature is 25°C
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  room_temperature: {
+    marginTop: 20,
+    fontSize: 22,
+    fontWeight: 'bold'
+  }
+});
+
+module.exports = ACControl;
--- presentational/GenericCircularSlider.js
+++ presentational/GenericCircularSlider.js
@@ -3,18 +3,20 @@
 import * as React from 'react';
 import { View, Text, StyleSheet, PanResponder } from 'react-native';
 
-import Svg, { Circle, Defs, LinearGradient, Stop } from 'react-native-svg';
+import Svg, { Path, Circle, Defs, LinearGradient as SvgLinearGradient, Stop }
+  from 'react-native-svg';
+import LinearGradient from 'react-native-linear-gradient';
+
+import type { LayoutType, StyleType } from './flowtypes';
 
 type PropsType = {
-  /* size of the circular arc in degrees */
-  arc: number,
-
   /* provide maximum and minimum inclusive range and round function */
   value?: number,
   maximum?: number,
   minimum?: number,
   round?: (value: number) => number,
-  onStart?: () => null,
+  formatText: (string) => string,
+  onStart?: (value: number) => null,
   /* onMove doesn't necessarily need to update value passed through props -
      circular slider live updates on it's own */
   onMove?: (value: number) => null,
@@ -22,8 +24,18 @@ type PropsType = {
      back to original value */
   onRelease?: (value: number) => null,
 
+  /* size of the circular arc in degrees */
+  arc: number,
+  diameter: number,
+
   /* override styling */
-  layout?: LayoutType,
+  knobGradient?: [string, string],
+  highlightGradient?: [string, string],
+  backgroundGradient?: [string, string],
+  backgroundStroke?: string,
+  knobWidth?: number,
+  arcWidth?: number,
+  arcMargin?: number,
 
   // TODO: for the future
   nightMode?: boolean
@@ -32,35 +44,56 @@ type PropsType = {
 type StateType = {
   touch: boolean,
   touch_value: number,
-  touch_start_value: number
-}
+  /* in radians */
+  touch_angle: number
+};
 
 class GenericCircularSlider extends React.Component<PropsType, StateType> {
 
   static defaultProps = {
-    arc: 270,
     value: 50,
     maximum: 100,
     minimum: 0,
     round: (value) => Math.round(value),
+    formatText: (string) => string,
     onStart: () => null,
     onMove: () => null,
     onRelease: () => null,
+    arc: 250,
+    diameter: 300,
+    knobGradient: ['#181B31', '#181B31'],
+    highlightGradient: ['#444444', '#444444'],
+    backgroundGradient: ['#2368AE', '#F03B4B'],
+    backgroundStroke: '#181B31',
+    knobDiameter: 70,
+    arcWidth: 30,
+    arcMargin: 5,
   };
 
   state = {
     touch: false,
     touch_value: 0,
-    touch_start_value: 0
+    touch_angle: 0
   };
 
-  _diameter: number = 300;
+  /* info: only calculated once */
+  _knob_layout: LayoutType | StyleType;
+  _container_layout: LayoutType;
 
   /* touch responder */
   _panResponder: Object;
 
-  componentWillMount() {
+  /* arc path */
+  _arc_path: string;
 
+  /* component x-axis and y-axis position relative to screen */
+  _x_pos: number;
+  _y_pos: number;
+
+  /* reference to container object used to obtain component position */
+  _container_ref: Object;
+
+  componentWillMount() {
     /* create touch responder */
     this._panResponder = PanResponder.create({
       onStartShouldSetPanResponder: (evt, gestureState) => true,
@@ -72,37 +105,242 @@ class GenericCircularSlider extends React.Component<PropsType, StateType> {
       onPanResponderMove: this._onPanResponderMove.bind(this),
       onPanResponderRelease: this._onPanResponderRelease.bind(this)
     });
+
+    this.createArc();
+    this.createContainerLayout();
+    this.createKnobLayout();
   }
 
-  _onPanResponderGrant() {
+  _onPanResponderGrant(evt: Object, gestureState: {x0: number, y0: number}) {
+    const { onStart } = this.props;
 
+    const touch_angle = this.calculateAngleFromCoord(gestureState.x0,
+      gestureState.y0);
+    const touch_value = this.calculateValueFromAngle(touch_angle);
+
+    this.setState({
+      touch: true,
+      touch_angle,
+      touch_value
+    });
+
+    /* call provided onStart handler */
+    onStart(touch_value);
   }
 
-  _onPanResponderMove() {
+  _onPanResponderMove(evt: Object, gestureState: {moveX: number,
+    moveY: number}) {
+    const { value, onMove } = this.props;
 
+    const touch_angle = this.calculateAngleFromCoord(gestureState.moveX,
+      gestureState.moveY);
+    const touch_value = this.calculateValueFromAngle(touch_angle);
+
+    this.setState({
+      touch_angle,
+      touch_value
+    });
+
+    /* if value has changed, call provided onMove handler */
+    if (value !== touch_value) {
+      onMove(touch_value);
+    }
   }
 
   _onPanResponderRelease() {
+    const { onRelease } = this.props;
+    const { touch_value } = this.state;
 
+    this.setState({
+      touch: false
+    });
+
+    /* call provided onRelease handler */
+    onRelease(touch_value);
+  }
+
+  calculateAngleFromCoord(x_touch: number, y_touch: number): number {
+    const { diameter, arc } = this.props;
+
+    /* normalize touch poitns to circle origin */
+    const touch_points = {
+      x: x_touch - diameter / 2 - this._x_pos,
+      y: y_touch - diameter / 2 - this._y_pos
+    };
+
+    /* calculate new angle in radians */
+    var angle = Math.atan2(touch_points.y, touch_points.x);
+
+    /* convert angle to degrees with where top of circle is 0, right is positive
+       and left is negative */
+    var angle_degress = (angle + Math.PI) * 180 / Math.PI - 90;
+    if (angle_degress > 180) {
+      angle_degress -= 360;
+    }
+
+    /* keep angle within bounds of arc */
+    if (angle_degress > arc / 2) {
+      angle = (arc / 2 - 90) * Math.PI / 180;
+    }
+
+    else if (angle_degress < arc / 2 * -1) {
+      angle = (arc / 2 * -1 - 90) * Math.PI / 180;
+    }
+
+    return angle;
+  }
+
+  calculateValueFromAngle(angle: number): number {
+    const { maximum, minimum, arc, round } = this.props;
+
+    /* convert angle to degrees with where top of circle is 0, right is positive
+       and left is negative */
+    var angle_degrees = (angle + Math.PI) * 180 / Math.PI - 90;
+    if (angle_degrees > 180) {
+      angle_degrees -= 360;
+    }
+
+    const ratio = arc / (maximum - minimum);
+    const angle_from_start = angle_degrees + arc / 2;
+    return round(minimum + angle_from_start / ratio);
+  }
+
+  calculateAngleFromValue(value: number): number {
+    const { maximum, minimum, arc } = this.props;
+
+    /* calculate the angle in degrees, where top of circle is 0 */
+    const angle_degrees = arc / (maximum - minimum) * (value - minimum) -
+      arc / 2;
+
+    return angle_degrees * Math.PI / 180 - Math.PI / 2;
+  }
+
+  calculateKnobPositionFromAngle(angle: number): {left: number, top: number} {
+    const { diameter, knobDiameter, arcWidth, arcMargin } = this.props;
+
+    const radius = diameter / 2 - knobDiameter / 2;
+    const overflow = (knobDiameter - arcWidth) / 2 - arcMargin;
+    return {
+      left: (radius + overflow) * Math.cos(angle) + radius,
+      top: (radius + overflow) * Math.sin(angle) + radius
+    }
+  }
+
+  createContainerLayout() {
+    const { diameter, arc, arcWidth } = this.props;
+
+    const angle = (360 - arc) * Math.PI / 180;
+
+    /* calculate height of circle segment that is not part of arc */
+    const extra_height = diameter / 2 * (1 - Math.cos(angle / 2));
+
+    this._container_layout = {
+      height: diameter - extra_height + arcWidth,
+      width: diameter
+    };
+  }
+
+  createKnobLayout() {
+    const { knobDiameter } = this.props;
+
+    /* calculate knob layout and style */
+    this._knob_layout = {
+      height: knobDiameter,
+      width: knobDiameter,
+      borderRadius: knobDiameter / 2
+    };
+  }
+
+  createArc() {
+    const { arc, diameter, arcWidth, arcMargin } = this.props;
+
+    /* calculate the start angle and end angle in radians, where 0 is the
+       very top of the circle */
+    const start_angle = (arc / 2 - 90) * Math.PI / 180;
+    const end_angle = (arc / 2 * -1 - 90) * Math.PI / 180;
+
+    /* calculate the actual radius of the middle of the arc stroke,
+       factoring in it's thickness and margin */
+    const radius = diameter / 2 - arcWidth / 2 - arcMargin;
+
+    /* calculate the cartesian start and end points */
+    const start = {
+      x: diameter / 2 + (radius * Math.cos(start_angle)),
+      y: diameter / 2 + (radius * Math.sin(start_angle))
+    };
+
+    const end = {
+      x: diameter / 2 + (radius * Math.cos(end_angle)),
+      y: diameter / 2 + (radius * Math.sin(end_angle))
+    };
+
+    /* set this flag to true if the arc is greater than 180 degrees
+       so the SVG library draws the bigger arc */
+    const large_arc_flag = (arc > 180) ? '1' : 0;
+
+    this._arc_path = [
+      'M', start.x, start.y,
+      'A', radius, radius, 0, large_arc_flag, 0, end.x, end.y
+    ].join(' ');
+  }
+
+  _measure() {
+    this._container_ref.measure((x, y, width, height, pageX, pageY) => {
+      this._x_pos = pageX;
+      this._y_pos = pageY;
+    });
   }
 
   render() {
+    const { diameter, highlightGradient, backgroundGradient,
+      backgroundStroke, knobDiameter, arcWidth, arcMargin, round, formatText }
+      = this.props;
+    var { value, knobGradient } = this.props;
+    const { touch, touch_angle, touch_value } = this.state;
 
-    const radius = this._diameter / 2;
-    const width = 50;
+
+    var knob_position: {left: number, top: number};
+    if (touch) {
+      knobGradient = highlightGradient;
+      knob_position = this.calculateKnobPositionFromAngle(touch_angle);
+      value = touch_value;
+    }
+    else {
+      const angle = this.calculateAngleFromValue(value);
+      knob_position = this.calculateKnobPositionFromAngle(angle);
+    }
 
     return (
-      <View style={[styles.container, {height: this._diameter, width: this._diameter}]}>
-        <Svg width={this._diameter} height={this._diameter}>
+      <View {...this._panResponder.panHandlers}
+        ref={c => this._container_ref = c}
+        onLayout={this._measure.bind(this)}
+        style={[styles.container, this._container_layout]}>
+        <Svg width={this._container_layout.width} height={this._container_layout.height}>
           <Defs>
-            <LinearGradient id={'gradient'} x1={0} y1={radius} x2={this._diameter} y2={radius}>
-              <Stop offset={'0'} stopColor={'#2368AE'} />
-              <Stop offset={'1'} stopColor={'#F03B4B'} />
-            </LinearGradient>
+            <SvgLinearGradient id={'gradient'}
+              x1={0} y1={diameter / 2} x2={diameter} y2={diameter / 2}>
+                <Stop offset={'0'} stopColor={backgroundGradient[0]} />
+                <Stop offset={'1'} stopColor={backgroundGradient[1]} />
+            </SvgLinearGradient>
           </Defs>
-          <Circle cx={radius} cy={radius} r={radius - width / 2} stroke={'url(#gradient)'} strokeWidth={width} fill={'none'}/>
-          {/* <Circle cx={radius} cy={radius} r={radius - width} stroke={'#0000FF'} strokeWidth={width} fill={'none'}/> */}
+
+          <Path d={this._arc_path}
+            stroke={backgroundStroke} strokeWidth={arcWidth + arcMargin * 2}
+              strokeLinecap={'round'} fill={'none'} />
+          <Path d={this._arc_path}
+            stroke={'url(#gradient)'} strokeWidth={arcWidth}
+              strokeLinecap={'round'} fill={'none'} />
         </Svg>
+
+        <LinearGradient {...this._panResponder.panHandlers}
+          colors={knobGradient}
+          start={{x: 1, y: 0}} end={{x: 0, y: 1}}
+          style={[styles.knob, this._knob_layout, knob_position]}>
+        </LinearGradient>
+
+        <Text style={styles.temperature}>
+          {formatText(value.toString())}
+        </Text>
       </View>
     );
   }
@@ -110,9 +348,17 @@ class GenericCircularSlider extends React.Component<PropsType, StateType> {
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  header: {
-
+  knob: {
+    position: 'absolute',
+  },
+  temperature: {
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    position: 'absolute',
+    fontSize: 50,
+    fontWeight: 'bold'
   }
 });
 
