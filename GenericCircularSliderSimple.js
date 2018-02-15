ff995346bb43b7f78489c05ff8134ba1f61cbd03
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2018-02-15 16:42:17 +0300

    Added simple controls for shitty UI

---
+++ GenericCircularSliderSimple.js
@@ -0,0 +1,392 @@
+/* @flow */
+
+import * as React from 'react';
+import { View, Text, StyleSheet, PanResponder } from 'react-native';
+
+import Svg, { Path, Circle, Defs, LinearGradient as SvgLinearGradient, Stop }
+  from 'react-native-svg';
+import LinearGradient from 'react-native-linear-gradient';
+
+import type { LayoutType, StyleType } from './flowtypes';
+
+type PropTypes = {
+  disabled?: boolean,
+
+  /* provide maximum and minimum inclusive range and round function */
+  value?: number,
+  maximum?: number,
+  minimum?: number,
+  round?: (value: number) => number,
+  onStart?: (value: number) => null,
+  /* onMove doesn't necessarily need to update value passed through props -
+     circular slider live updates on it's own */
+  onMove?: (value: number) => null,
+  /* onRelease must pass updated value through props or else slider will pop
+     back to original value */
+  onRelease?: (value: number) => null,
+
+  /* size of the circular arc in degrees */
+  arc: number,
+  diameter: number,
+
+  /* override styling */
+  knobGradient?: [string, string],
+  knobDisabledGradient?: [string, string],
+  highlightGradient?: [string, string],
+  backgroundGradient?: [string, string],
+  backgroundStroke?: string,
+  knobWidth?: number,
+  arcWidth?: number,
+  arcMargin?: number,
+
+  // TODO: for the future
+  nightMode?: boolean
+};
+
+type StateType = {
+  touch: boolean,
+  touch_value: number,
+  /* in radians */
+  touch_angle: number
+};
+
+class GenericCircularSliderSimple extends React.Component<PropTypes, StateType> {
+
+  static defaultProps = {
+    disable: false,
+    value: 50,
+    maximum: 100,
+    minimum: 0,
+    round: (value) => Math.round(value),
+    onStart: () => null,
+    onMove: () => null,
+    onRelease: () => null,
+    arc: 300,
+    diameter: 300,
+    knobGradient: ['#000000', '#000000'],
+    highlightGradient: ['#000000', '#000000'],
+    knobDisabledGradient: ['#000000', '#000000'],
+    backgroundGradient: ['#ff0000', '#3B9FFF'],
+    backgroundStroke: '#666666',
+    knobDiameter: 40,
+    arcWidth: 20,
+    arcMargin: 2,
+  };
+
+  state = {
+    touch: false,
+    touch_value: 0,
+    touch_angle: 0
+  };
+
+  /* info: only calculated once */
+  _knob_layout: LayoutType | StyleType;
+  _svg_layout: LayoutType | StateType;
+  _container_layout: LayoutType;
+
+  /* touch responder */
+  _panResponder: Object;
+
+  /* animated angle */
+  _animated_angle: Object;
+  _old_angle: number;
+
+  /* component x-axis and y-axis position relative to screen */
+  _x_pos: number;
+  _y_pos: number;
+
+  /* reference to container object used to obtain component position */
+  _container_ref: Object;
+
+  componentWillMount() {
+    /* create touch responder */
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: this._onPanResponderGrant.bind(this),
+      onPanResponderMove: this._onPanResponderMove.bind(this),
+      onPanResponderRelease: this._onPanResponderRelease.bind(this)
+    });
+    this.createSvgAndContainerLayout();
+    this.createKnobLayout();
+  }
+
+  _onPanResponderGrant(evt: Object, gestureState: {x0: number, y0: number}) {
+    const { onStart } = this.props;
+
+    /* get position of element on screen for touch offset calculation */
+    this._measure(() => {
+      const touch_angle = this.calculateAngleFromCoord(gestureState.x0,
+        gestureState.y0);
+      const touch_value = this.calculateValueFromAngle(touch_angle);
+
+      this.setState({
+        touch: true,
+        touch_angle,
+        touch_value
+      });
+
+      /* call provided onStart handler */
+      onStart(touch_value);
+    });
+  }
+
+  _onPanResponderMove(evt: Object, gestureState: {moveX: number,
+    moveY: number}) {
+    const { value, onMove, minimum, maximum } = this.props;
+    const { touch_angle, touch_value } = this.state;
+
+    var new_touch_angle = this.calculateAngleFromCoord(gestureState.moveX,
+      gestureState.moveY);
+    var new_touch_value = this.calculateValueFromAngle(new_touch_angle);
+
+    if (Math.abs(new_touch_value - touch_value) > (maximum - minimum) / 2) {
+      return;
+    }
+
+    this.setState({
+      touch_angle: new_touch_angle,
+      touch_value: new_touch_value
+    });
+
+    /* if value has changed, call provided onMove handler */
+    if (value !== touch_value) {
+      onMove(touch_value);
+    }
+  }
+
+  _onPanResponderRelease() {
+    const { onRelease } = this.props;
+    const { touch_value } = this.state;
+
+    this.setState({
+      touch: false
+    });
+
+    /* call provided onRelease handler */
+    onRelease(touch_value);
+  }
+
+  calculateAngleFromCoord(x_touch: number, y_touch: number): number {
+    const { diameter, arc } = this.props;
+
+    /* normalize touch poitns to circle origin */
+    const touch_points = {
+      x: x_touch - diameter / 2 - this._x_pos,
+      y: y_touch - diameter / 2 - this._y_pos
+    };
+
+    /* calculate new angle in radians */
+    var angle = Math.atan2(touch_points.y, touch_points.x);
+
+    /* convert angle to degrees with where top of circle is 0, right is positive
+       and left is negative */
+    var angle_degress = (angle + Math.PI) * 180 / Math.PI - 90;
+    if (angle_degress > 180) {
+      angle_degress -= 360;
+    }
+
+    /* keep angle within bounds of arc */
+    if (angle_degress > arc / 2) {
+      angle = (arc / 2 - 90) * Math.PI / 180;
+    }
+
+    else if (angle_degress < arc / 2 * -1) {
+      angle = (arc / 2 * -1 - 90) * Math.PI / 180;
+    }
+
+    return angle;
+  }
+
+  calculateValueFromAngle(angle: number): number {
+    const { touch_value } = this.state;
+    const { maximum, minimum, arc, round } = this.props;
+
+    /* convert angle to degrees with where top of circle is 0, right is positive
+       and left is negative */
+    var angle_degrees = (angle + Math.PI) * 180 / Math.PI - 90;
+    if (angle_degrees > 180) {
+      angle_degrees -= 360;
+    }
+
+    const ratio = arc / (maximum - minimum);
+    const angle_from_start = angle_degrees + arc / 2;
+    return round(minimum + angle_from_start / ratio);
+  }
+
+  calculateAngleFromValue(value: number): number {
+    const { maximum, minimum, arc } = this.props;
+
+    /* calculate the angle in degrees, where top of circle is 0 */
+    const angle_degrees = arc / (maximum - minimum) * (value - minimum) -
+      arc / 2;
+
+    return angle_degrees * Math.PI / 180 - Math.PI / 2;
+  }
+
+  calculateKnobPositionFromAngle(angle: number): {left: number, top: number} {
+    const { diameter, knobDiameter, arcWidth, arcMargin } = this.props;
+
+    const radius = diameter / 2 - knobDiameter / 2;
+    const overflow = (knobDiameter - arcWidth) / 2 - arcMargin;
+    return {
+      left: (radius + overflow) * Math.cos(angle) + radius + this._svg_layout.left,
+      top: (radius + overflow) * Math.sin(angle) + radius + this._svg_layout.top
+    }
+  }
+
+  createSvgAndContainerLayout() {
+    const { diameter, arc, arcWidth, knobDiameter } = this.props;
+
+    const angle = (360 - arc) * Math.PI / 180;
+
+    /* calculate height of circle segment that is not part of arc */
+    const extra_height = diameter / 2 * (1 - Math.cos(angle / 2));
+
+    this._svg_layout = {
+      height: diameter - extra_height + arcWidth,
+      width: diameter,
+      top: (knobDiameter - arcWidth) / 4 + 1,
+      left: (knobDiameter - arcWidth) / 4 + 1,
+    };
+
+    this._container_layout = {
+      height: this._svg_layout.height + this._svg_layout.top * 2,
+      width: this._svg_layout.width + this._svg_layout.left * 2
+    };
+  }
+
+  createKnobLayout() {
+    const { knobDiameter } = this.props;
+
+    /* calculate knob layout and style */
+    this._knob_layout = {
+      height: knobDiameter,
+      width: knobDiameter,
+      borderRadius: knobDiameter / 2
+    };
+  }
+
+  createArc(start_angle: number, end_angle: number) {
+    const { diameter, arcWidth, arcMargin } = this.props;
+
+    /* calculate the actual radius of the middle of the arc stroke,
+       factoring in it's thickness and margin */
+    const radius = diameter / 2 - arcWidth / 2 - arcMargin;
+
+    /* calculate the cartesian start and end points */
+    const start = {
+      x: diameter / 2 + (radius * Math.cos(start_angle)),
+      y: diameter / 2 + (radius * Math.sin(start_angle))
+    };
+
+    const end = {
+      x: diameter / 2 + (radius * Math.cos(end_angle)),
+      y: diameter / 2 + (radius * Math.sin(end_angle))
+    };
+
+    /* set this flag to true if the arc is greater than 180 degrees
+       so the SVG library draws the bigger arc */
+    var total_angle = Math.abs(start_angle - end_angle);
+    const large_arc_flag = (total_angle > Math.PI) ? '1' : 0;
+
+    return [
+      'M', start.x, start.y,
+      'A', radius, radius, 0, large_arc_flag, 0, end.x, end.y
+    ].join(' ');
+  }
+
+  _measure(callback) {
+    this._container_ref.measure((x, y, width, height, pageX, pageY) => {
+      this._x_pos = pageX;
+      this._y_pos = pageY;
+
+      if (typeof callback == 'function') {
+        callback();
+      }
+    });
+  }
+
+  calculateAngleAnimation() {
+
+  }
+
+  componentDidUpdate(prevProps: PropTypes) {
+
+    this._old_angle = prevProps.value;
+    this.calculateAngleAnimation();
+  }
+
+  render() {
+    const { arc, diameter, highlightGradient, backgroundGradient,
+      backgroundStroke, knobDiameter, arcWidth, arcMargin, round, disabled,
+      knobDisabledGradient } = this.props;
+    var { value, knobGradient } = this.props;
+    const { touch, touch_angle, touch_value } = this.state;
+
+    var knob_position: {left: number, top: number};
+    var angle = this.calculateAngleFromValue(value);
+    if (touch) {
+      knobGradient = highlightGradient;
+      knob_position = this.calculateKnobPositionFromAngle(touch_angle);
+      value = touch_value;
+    } else {
+      knob_position = this.calculateKnobPositionFromAngle(angle);
+    }
+
+    var panHandlers = {};
+    if (!disabled) {
+      panHandlers = this._panResponder.panHandlers;
+    } else {
+      knobGradient = knobDisabledGradient;
+    }
+
+
+    /* calculate the start angle and end angle in radians, where 0 is the
+       very top of the circle */
+    var knobAngle = touch ? touch_angle : angle;
+    const start_angle = (arc / 2 - 90) * Math.PI / 180;
+    const end_angle = (arc / 2 * -1 - 90) * Math.PI / 180;
+    if (knobAngle > start_angle)
+        knobAngle -= Math.PI * 2;
+    var arcLeft = this.createArc(start_angle, knobAngle);
+    var arcRight = this.createArc(knobAngle, end_angle);
+
+    return (
+      <View {...panHandlers}
+        ref={c => this._container_ref = c}
+        onLayout={this._measure.bind(this)}
+        style={this._container_layout}>
+        <Svg width={this._svg_layout.width} height={this._svg_layout.height}
+          style={[styles.svg,
+            {top: this._svg_layout.top, left: this._svg_layout.left, opacity: disabled ? 0.5 : 1}]}>
+          <Path d={arcLeft}
+            stroke={backgroundGradient[0]} strokeWidth={arcWidth}
+              strokeLinecap={'round'} fill={'none'} />
+          <Path d={arcRight}
+            stroke={backgroundGradient[1]} strokeWidth={arcWidth}
+              strokeLinecap={'round'} fill={'none'} />
+        </Svg>
+
+        <LinearGradient colors={knobGradient}
+          start={{x: 1, y: 0}} end={{x: 0, y: 1}}
+          style={[styles.knob, this._knob_layout, knob_position]}>
+        </LinearGradient>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  svg: {
+    position: 'absolute'
+  },
+  knob: {
+    position: 'absolute',
+  }
+});
+
+module.exports = GenericCircularSliderSimple;
---
+++ GenericSliderSimple.js
@@ -0,0 +1,400 @@
+/* @flow */
+
+import * as React from 'react';
+import { View, Image, Text, PanResponder, StyleSheet } from 'react-native';
+
+import LinearGradient from 'react-native-linear-gradient';
+
+import type { LayoutType, StyleType } from './flowtypes';
+
+type PropTypes = {
+  /* provide maximum and minimum inclusive value range and round function */
+  value?: number,
+  maximum?: number,
+  minimum?: number,
+  round?: (value: number) => number,
+  textGenerator?: (value: number) => string,
+  onStart?: () => null,
+  /* onMove doesn't necessarily need to update value passed through props -
+     slider live updates on it's own */
+  onMove?: (value: number) => null,
+  /* onRelease must pass updated value through props or else slider will pop
+     back to original value */
+  onRelease?: (value: number) => null,
+
+  /* override styling */
+  orientation?: 'vertical' | 'horizontal',
+  icon?: number, /* this must be result of require(<image>) */
+  layout?: LayoutType,
+  showValue?: boolean,
+  fontColor?: string,
+  sliderGradient?: [string, string],
+  backgroundColor?: string,
+  iconBackgroundColor?: string,
+  sliderMargin?: number,
+
+  // TODO: for the futrue
+  nightMode?: boolean
+};
+
+type StateType = {
+  touch: boolean,
+  touch_value: number,
+  touch_start_value: number
+};
+
+class GenericSliderSimple extends React.Component<PropTypes, StateType> {
+
+  static defaultProps = {
+    orientation: 'horizontal',
+    value: 50,
+    maximum: 100,
+    minimum: 0,
+    round: (value: number) => Math.round(value),
+    textGenerator: (value: number) => value,
+    onStart: () => null,
+    onMove: () => null,
+    onRelease: () => null,
+    showValue: false,
+    fontColor: '#FFFFFF',
+    sliderGradient: ['#3B9FFF', '#3B9FFF'],
+    highlightGradient: ['#4BAFFF', '#4BAFFF'],
+    backgroundColor: '#888888',
+    iconBackgroundColor: '#00000000',
+    sliderMargin: 2,
+    nightMode: true,
+  };
+
+  state = {
+    touch: false,
+    touch_value: 0,
+    touch_start_value: 0
+  };
+
+  /* default height and width for slider if non provided through props */
+  _default_height: number = 70;
+  _default_width: number = 250;
+
+  _container_layout: LayoutType | StyleType;
+  _slider_mask: LayoutType | StyleType;
+  _slider_layout: StyleType;
+  _ratio: number;
+  _icon_container_layout: LayoutType | StyleType;
+  _icon_layout: LayoutType;
+
+  /* touch responder */
+  _panResponder: Object;
+
+  /* component x-axis and y-axis position relative to screen */
+  _x_pos: number;
+  _y_pos: number;
+
+  /* reference to container object used to obtain component position */
+  _container_ref: Object;
+
+  componentWillMount() {
+    /* create touch responder */
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: this._onPanResponderGrant.bind(this),
+      onPanResponderMove: this._onPanResponderMove.bind(this),
+      onPanResponderRelease: this._onPanResponderRelease.bind(this)
+    });
+  }
+
+  calculateCurrentTouch(evt: Object, gestureState: {dx: number, dy: number}, is_first_touch: boolean) {
+    const { orientation, maximum, minimum, round, onMove } = this.props;
+    var { touch_value, touch_start_value } = this.state;
+
+    if (is_first_touch) {
+        touch_start_value = (gestureState.x0 - this._x_pos) / this._ratio;
+    }
+
+    /* calculate gesture distance and limit value to remain within range */
+    var new_value: number;
+    if (orientation === 'horizontal') {
+      new_value = touch_start_value + (gestureState.dx / this._ratio);
+    }
+    else {
+      new_value = touch_start_value - (gestureState.dy / this._ratio);
+    }
+    var rounded_new_value: number = round(new_value);
+
+    /* keep value within set bounds */
+    if (new_value > maximum) {
+      rounded_new_value = maximum;
+    }
+
+    else if (new_value < minimum) {
+      rounded_new_value = minimum;
+    }
+    return rounded_new_value;
+  }
+
+  _onPanResponderGrant(evt: Object, gestureState: {dx: number, dy: number}) {
+    const { orientation, maximum, minimum, round, onMove } = this.props;
+    var { touch_value, touch_start_value } = this.state;
+    const { value, onStart } = this.props;
+
+    var rounded_new_value = this.calculateCurrentTouch(evt, gestureState, true);
+
+    this.setState({
+      touch: true,
+      touch_start_value: rounded_new_value,
+      touch_value: rounded_new_value,
+    });
+
+    /* call provided onStart handler */
+    onStart();
+
+    /* if rounded value has changed, call provided onMove handler */
+    if (rounded_new_value !== round(touch_value)) {
+      onMove(rounded_new_value);
+    }
+  }
+
+  _onPanResponderMove(evt: Object, gestureState: {dx: number, dy: number}) {
+    const { orientation, maximum, minimum, round, onMove } = this.props;
+    var { touch_value, touch_start_value } = this.state;
+
+    var rounded_new_value = this.calculateCurrentTouch(evt, gestureState);
+
+    this.setState({
+      touch_value: rounded_new_value
+    });
+
+    /* if rounded value has changed, call provided onMove handler */
+    if (rounded_new_value !== round(touch_value)) {
+      onMove(rounded_new_value);
+    }
+  }
+
+  _onPanResponderRelease() {
+    const { round, onRelease } = this.props;
+    const { touch_value } = this.state;
+
+    this.setState({
+      touch: false
+    });
+
+    /* call provided onRelease handler */
+    onRelease(round(touch_value));
+  }
+
+  _measure(callback) {
+    this._container_ref.measure((x, y, width, height, pageX, pageY) => {
+      this._x_pos = pageX;
+      this._y_pos = pageY;
+
+      if (typeof callback == 'function') {
+        callback();
+      }
+    });
+  }
+
+  calculateLayout() {
+    const { orientation, sliderMargin, icon } = this.props;
+    var { layout } = this.props;
+
+    /* layout has not been passed through props, fallback to default */
+    if (!layout) {
+      layout = {
+        height: (orientation === 'horizontal') ?
+          this._default_height : this._default_width,
+        width: (orientation === 'horizontal') ?
+          this._default_width : this._default_height
+      };
+    }
+
+    /* calculate the layout of the slider container */
+    this._container_layout = {
+      height: layout.height,
+      width: layout.width,
+      borderRadius: 5, //layout.width / 2
+    };
+
+    /* calculate icon layout */
+    if (icon) {
+      this._container_layout.marginLeft = layout.height / 2 * -1;
+      this._container_layout.width -= layout.height;
+    }
+
+    this._icon_container_layout = {
+      height: layout.height,
+      width: layout.height * 3 / 2,
+      borderTopLeftRadius: 5, //layout.height / 2,
+      borderBottomLeftRadius: 5, //layout.height / 2
+    };
+
+    this._icon_layout = {
+      height: layout.height * 3 / 4,
+      width: layout.height * 3 / 4,
+      marginRight: layout.height / 2
+    };
+
+    /* calculate the layout of the slider mask */
+    this._slider_mask = {
+      height: this._container_layout.height - sliderMargin * 2,
+      width: this._container_layout.width - sliderMargin * 2,
+      top: sliderMargin,
+      left: sliderMargin,
+      borderRadius: 5,//(this._container_layout.width - sliderMargin * 2) / 2
+    };
+
+    /* calculate the border radii of the slider itself depending on
+       orientation */
+    if (orientation === 'horizontal') {
+      this._slider_layout = {
+        borderTopRightRadius: this._slider_mask.borderRadius,
+        borderBottomRightRadius: this._slider_mask.borderRadius,
+      }
+    }
+    else {
+      this._slider_layout = {
+        borderTopRightRadius: this._slider_mask.borderRadius,
+        borderTopLeftRadius: this._slider_mask.borderRadius,
+      };
+    }
+  }
+
+  calculateSliderRatio() {
+    const { orientation, maximum, minimum, round, sliderMargin } = this.props;
+
+    if (orientation === 'horizontal') {
+      this._ratio = (this._container_layout.width - sliderMargin * 2) /
+        (maximum - minimum);
+    }
+    else {
+      this._ratio = (this._container_layout.height - sliderMargin * 2) /
+        (maximum - minimum);
+    }
+  }
+
+  render() {
+    const { orientation, minimum, round, textGenerator, fontColor, highlightGradient,
+      backgroundColor, showValue, icon, iconBackgroundColor } = this.props;
+    var { value, sliderGradient } = this.props;
+    const { touch, touch_value } = this.state;
+
+    /* recalculate layout and ratio */
+    this.calculateLayout();
+    this.calculateSliderRatio();
+
+    /* if touches began, override provided value */
+    if (touch) {
+      value = touch_value;
+      sliderGradient = highlightGradient;
+    }
+
+    /* calculate the size of slider */
+    const slider_size: LayoutType = {};
+    if (orientation === 'horizontal') {
+      const width = (value - minimum) * this._ratio - 1;
+      if (width < this._slider_mask.height) {
+        slider_size.width = this._slider_mask.height;
+        slider_size.left = width - this._slider_mask.height;
+      } else {
+        slider_size.width = width;
+      }
+    }
+
+    else {
+      const height = (value - minimum) * this._ratio;
+      if (height < this._slider_mask.width) {
+        slider_size.height = this._slider_mask.width;
+        slider_size.bottom = height - this._slider_mask.width;
+      } else {
+        slider_size.height = height;
+      }
+    }
+
+    var value_text = null;
+    if (showValue) {
+      value_text = <View style={styles.value_container}>
+        <Text style={[styles.value_text, {color: fontColor}]}>
+          {textGenerator(round(value))}
+        </Text>
+      </View>
+    }
+
+    var knob = (
+        <View style={{
+            position: 'absolute',
+            width: 40,
+            height: this._container_layout.height*0.7,
+            top: this._container_layout.height*0.15,
+            left: Math.min(slider_size.width  + (slider_size.left || 0), this._slider_mask.width-40),
+            backgroundColor: '#000000',
+            borderRadius: 2
+        }}>
+        </View>
+    );
+
+    return (
+      <View {...this._panResponder.panHandlers}
+        style={styles.container}>
+        {(icon) ? <View style={[this._icon_container_layout,
+          styles.icon_container, {backgroundColor: iconBackgroundColor}]}>
+            <Image style={this._icon_layout} source={icon} />
+          </View> : null}
+
+          <View style={[this._container_layout]}
+            onLayout={this._measure.bind(this)}
+            ref={c => this._container_ref = c}>
+            <View style={[styles.slider_mask, this._slider_mask]}>
+              <View style={{width: '100%', height: this._container_layout.height/2, marginTop: this._container_layout.height/4, backgroundColor}}>
+                <LinearGradient colors={sliderGradient}
+                  start={{x: 1, y: 0}} end={{x: 0, y: 1}}
+                  style={[styles.slider, this._slider_layout, slider_size]}>
+                </LinearGradient>
+                {value_text}
+              </View>
+              {knob}
+            </View>
+          </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row'
+  },
+  icon_container: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  slider_mask: {
+    position: 'absolute',
+    overflow: 'hidden',
+  },
+  slider: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    bottom: 0,
+    overflow: 'hidden'
+  },
+  value_container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  value_text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0)'
+  }
+});
+
+module.exports = GenericSliderSimple;
